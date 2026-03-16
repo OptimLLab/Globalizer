c8752ef766659f7e5e2c7afd6fd81696cc17879e
Лебедев Илья Генадьевич <ilya.lebedev@unn.ru>
2026-03-16 15:42:34 +0300

    Автоматизация настройки параметров

---
+++ examples/Main.cpp
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////
+//                                                                         //
+//             LOBACHEVSKY STATE UNIVERSITY OF NIZHNY NOVGOROD             //
+//                                                                         //
+//                       Copyright (c) 2025 by UNN.                        //
+//                          All Rights Reserved.                           //
+//                                                                         //
+//  File:      HDOptimizationMain.cpp                                      //
+//                                                                         //
+//  Purpose:   Console version of Globalizer system                        //
+//                                                                         //
+//  Author(s): Lebedev I., Barkalov K.                                     //
+//                                                                         //
+/////////////////////////////////////////////////////////////////////////////
+
+
+#include "Globalizer.h"
+
+// ------------------------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+  GlobalizerInitialization(argc, argv);
+
+#ifdef _GLOBALIZER_BENCHMARKS
+  GlobalOptimizationProblemManager manager;
+  IGlobalOptimizationProblem* problem = 0;
+  if (InitProblemGlobalizerBenchmarks(manager, problem))
+  {
+    print << "Error during problem initialization\n";
+    return 0;
+  }
+
+  SolutionResult* result = GlobalizerSolveProblem(problem);
+
+#endif
+
+
+  MPI_Finalize();
+  return 0;
+}
+// - end of file ----------------------------------------------------------------------------------

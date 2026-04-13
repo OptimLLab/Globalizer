c9f51fa7fa4ee92f1f98ed229c87a744a6f82de9
kegorov0902 <72626550+kegorov0902@users.noreply.github.com>
2026-04-13 10:10:14 +0300

    delete_trash
    Delete build-related files

--- gen/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Globalizer/fork/Globalizer/tests
-# Build directory: D:/Globalizer/fork/Globalizer/gen/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[all_tests]=] "D:/Globalizer/fork/Globalizer/_bin/GLOBALIZER_tests")
-set_tests_properties([=[all_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/Globalizer/fork/Globalizer/tests/CMakeLists.txt;29;add_test;D:/Globalizer/fork/Globalizer/tests/CMakeLists.txt;0;")
